3957b116522ab2631eeef059f8271f7b3d2c98a1
krostyslav <rostyslav.kryvyy@gmail.com>
2018-03-07 21:21:52 +0200

    update bootstap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
+
 import {ChartsModule} from 'ng2-charts';
 import {MomentModule} from 'angular2-moment';
 import {NgProgressModule} from 'ngx-progressbar';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {AppComponent} from './app.component';
 import {InfoComponent} from './components/info/info.component';
@@ -16,6 +18,7 @@ import {AnalysisTweetsComponent} from './components/analysis-tweet/analysis-twee
 import {FooterComponent} from './components/footer/footer.component';
 
 import {HttpService} from './services/http-service.service';
+import {TokenInterceptor} from './auth/token.interceptor';
 import {ROUTES} from './app.routing';
 
 @NgModule({
@@ -33,12 +36,18 @@ import {ROUTES} from './app.routing';
     RouterModule.forRoot(ROUTES),
     ChartsModule,
     MomentModule,
+    NgbModule.forRoot(),
     NgProgressModule,
     HttpModule,
     HttpClientModule
   ],
   providers: [
-    HttpService
+    HttpService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [ AppComponent ]
 })
